463079ab3bb252b7db391fc7cfb06c91dd810d8e
ajnura <ainura.zhanabidinkyzy.ch@gmail.com>
2022-05-05 18:35:10 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        triangle tri=new triangle();
+        tri.a=6;
+        tri.b=6;
+        tri.c=7;
+
+        tri.area();
+
+
+
+
+
+
+    }
+}
---
+++ src/com/company/triangle.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class triangle {
+    int a;
+    int b;
+    int c;
+
+
+    void area(){
+        System.out.println(a*b*c);
+
+    }
+}
